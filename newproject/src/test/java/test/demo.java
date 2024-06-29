3de40aff659a0ab14bca18e381ecd5d61e3bd818
Rupesh <doshir04@gmail.com>
2024-06-29 14:04:29 +0530

    FIRST COMMIT

---
+++ newproject/src/test/java/test/demo.java
@@ -0,0 +1,38 @@
+package test;
+
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class demo {
+ 	 WebDriver driver;
+	 FileOutputStream fos;
+	 Logger logger 
+     = Logger.getLogger(demo.class.getName()); 
+	 @SuppressWarnings("restriction")
+	@Test
+	public void display() throws InterruptedException, IOException
+	{
+	  WebDriverManager.chromedriver().setup();
+	  driver=new ChromeDriver();
+	  driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+	  driver.manage().window().maximize();
+	  logger.info("trying to click on radio button");
+		  driver.findElement(By.xpath("//input[@value=\"radio1\"]")).click();
+	    
+
+	 Thread.sleep(5000);
+	 driver.close();
+	 
+	}
+     
+}
